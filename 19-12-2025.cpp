c0ab3189d8f60f04cab5d4420974f84d07e2d7b9
AnaKoDiK <Viking64rus@gmail.com>
2025-12-19 13:21:41 +0300

    Update 19-12-2025.cpp

--- 19-12-2025.cpp
+++ 19-12-2025.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+\\task 1
+int total(int price, int quantity)
+{
+    return price * quantity;
+}
+int main()
+{
+    int price = 500;
+    int quantity = 5;
+    cout << "Итоговая стоимость: " << total(price, quantity) << endl;
+    cout << "Исходная цена: " << price << endl;
+    cout << "Исходное количество: " << quantity << endl;
+    return 0;
+}
 \\task 3
 
 void print(string name,  int age = 14)
